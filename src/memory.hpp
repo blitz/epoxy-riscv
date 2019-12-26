4f72028b1f529966bcf6276950b0de80406a8303
Julian Stecklina <js@alien8.de>
2019-12-26 17:23:36 +0100

    Add simple userspace binary

--- src/memory.hpp
+++ src/memory.hpp
@@ -1,4 +1,11 @@
 #pragma once
 
+// General constants
+#define PAGE_SHIFT 12
+#define PAGE_SIZE  4096
+
 // The kernel starts at the top 2GB of address space
 #define KERNEL_START 0xffffffff80000000
+
+// Userspace apps start at 64K
+#define USER_START 0x0000000000010000
