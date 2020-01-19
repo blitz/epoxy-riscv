ad2f2166f1cf0beaa1faca70696dabc98fcd45c8
Julian Stecklina <js@alien8.de>
2020-01-19 23:56:04 +0100

    Minor style fixes

--- src/kern/include/kobject.hpp
+++ src/kern/include/kobject.hpp
@@ -7,18 +7,20 @@ class thread;
 class syscall_args;
 
 class kobject {
+protected:
+  constexpr kobject() = default;
 public:
   virtual syscall_result_t invoke(thread *thread, syscall_args const &args) = 0;
 };
 
 // A primitive logging system call.
 class klog_kobject final : public kobject {
+public:
   syscall_result_t invoke(thread *thread, syscall_args const &args) override;
 };
-static_assert(sizeof(klog_kobject) == sizeof(kobject));
 
 // Graceful exit from a thread.
 class exit_kobject final : public kobject {
+public:
   [[noreturn]] syscall_result_t invoke(thread *thread, syscall_args const &args) override;
 };
-static_assert(sizeof(exit_kobject) == sizeof(kobject));
