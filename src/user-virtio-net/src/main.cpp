883b44fd30b3439252edb10fd67680d7ac95e489
Julian Stecklina <js@alien8.de>
2020-03-08 20:33:19 +0100

    Move iterator helpers into common header

--- src/user-virtio-net/src/main.cpp
+++ src/user-virtio-net/src/main.cpp
@@ -2,6 +2,8 @@
 #include "format.hpp"
 #include "pci_device.hpp"
 
+#include <esl/iterators.hpp>
+
 extern "C" void main();
 
 namespace {
@@ -59,130 +61,6 @@ namespace {
 
   };
 
-  template <typename predicate_type,
-	    typename range_type>
-  class filter_range {
-    predicate_type predicate;
-    range_type &range;
-
-    using range_it = decltype(range.begin());
-
-  public:
-
-    class filtered_it {
-      predicate_type predicate;
-
-      range_it source_it;
-      range_it end_it;
-
-    public:
-
-      auto operator*()
-      {
-	return *source_it;
-      }
-
-      filtered_it &operator++()
-      {
-	do {
-	  ++source_it;
-	} while (source_it != end_it and not predicate(*source_it));
-
-	return *this;
-      }
-
-      bool operator==(filtered_it const &other) const
-      {
-	return source_it == other.source_it;
-      }
-
-      bool operator!=(filtered_it const &other) const
-      {
-	return source_it != other.source_it;
-      }
-
-      filtered_it(predicate_type const &predicate_, range_it const &source_it_, range_it const &end_it_)
-	: predicate{predicate_}, source_it{source_it_}, end_it{end_it_}
-      {
-	while (source_it != end_it and not predicate(*source_it)) {
-	  ++source_it;
-	}
-      }
-    };
-
-    filtered_it begin()
-    {
-      return {predicate, range.begin(), range.end()};
-    }
-
-    filtered_it end()
-    {
-      return {predicate, range.end(), range.end()};
-    }
-
-    filter_range(range_type &range_, predicate_type const &predicate_)
-      : predicate{predicate_}, range{range_}
-    {}
-  };
-
-  template <typename transform_type,
-	    typename range_type>
-  class transform_range {
-    transform_type transform;
-    range_type &range;
-
-    using range_it = decltype(range.begin());
-
-  public:
-
-    class transformed_it {
-      transform_type transform;
-
-      range_it source_it;
-
-    public:
-
-      auto operator*()
-      {
-	return transform(*source_it);
-      }
-
-      transformed_it &operator++()
-      {
-	++source_it;
-	return *this;
-      }
-
-      bool operator==(transformed_it const &other) const
-      {
-	return source_it == other.source_it;
-      }
-
-      bool operator!=(transformed_it const &other) const
-      {
-	return source_it != other.source_it;
-      }
-
-      transformed_it(transform_type const &transform_, range_it const &source_it_)
-	: transform{transform_}, source_it{source_it_}
-      {}
-    };
-
-    transformed_it begin()
-    {
-      return {transform, range.begin()};
-    }
-
-    transformed_it end()
-    {
-      return {transform, range.end()};
-    }
-
-    transform_range(range_type &range_, transform_type const &transform_)
-      : transform{transform_}, range{range_}
-    {}
-  };
-
 } // anonymous namespace
 
 void main()
@@ -192,11 +70,12 @@ void main()
   virtio_net_device virtio_net {virtio_net_pci_cfg};
 
   auto cap_list {virtio_net.get_cap_list()};
-  auto filtered_cap_list {filter_range(cap_list, &virtio_vendor_pci_cap::converts_from)};
+  auto filtered_cap_list {esl::filter_range(cap_list, &virtio_vendor_pci_cap::converts_from)};
 
   for (auto const vendor_cap
-	 : transform_range(filtered_cap_list,
-			   [] (pci_device::pci_cap const &cap) { return static_cast<virtio_vendor_pci_cap>(cap); })) {
+	 : esl::transform_range(filtered_cap_list,
+				[] (pci_device::pci_cap const &cap) {
+				  return static_cast<virtio_vendor_pci_cap>(cap); })) {
     format("cfg_type=", vendor_cap.get_cfg_type(),
 	   " bar=",     vendor_cap.get_bar_no(),
 	   " offset=",  vendor_cap.get_bar_offset(),
