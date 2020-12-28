be3726193c861078e15f28d679bcad901d017ccb
Julian Stecklina <js@alien8.de>
2020-12-28 19:05:15 +0100

    Remove demo exception

--- src/user-hello/src/main.cpp
+++ src/user-hello/src/main.cpp
@@ -1,18 +1,7 @@
 #include <cstdio>
 
-struct hello_world_exception {
-};
-
-int test()
-{
-  throw hello_world_exception {};
-}
-
 int main()
 {
   printf("Hello World!\n");
-
-  test();
-
   return 0;
 }
