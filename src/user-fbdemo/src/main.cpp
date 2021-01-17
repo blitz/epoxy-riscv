58bd027db72142e078d17c7bfe3ffb293418a1c2
Julian Stecklina <js@alien8.de>
2021-01-17 01:22:01 +0100

    Make user-fbdemo compatible to new generated resource declarations

--- src/user-fbdemo/src/main.cpp
+++ src/user-fbdemo/src/main.cpp
@@ -1,37 +1,27 @@
 #include <cstdint>
-#include <cstring>
 #include <cstdio>
+#include <cstring>
+#include <tuple>
+
+#include "resources.hpp"
 
 namespace
 {
-// TODO We have to hardcode the virtual addresses here for now. In
-// the future epoxy-harden should generate a nice header with
-// virtual addresses of shared memory regions.
-//
-// See virtioNetAddressSpace in user-hello.dhall.
-auto const framebuffer_raw {reinterpret_cast<uint16_t volatile *>(0x10000000)};
-uint32_t const framebuffer_width = 640;
-uint32_t const framebuffer_height = /*480*/240;
-uint32_t const framebuffer_stride = 1280;
-
-const int WIDTH = framebuffer_width;
-const int HEIGHT = framebuffer_height;
-
 const uint8_t FONT[256 * 8] = {
-  #include "font.inc"
+#include "font.inc"
 };
-
 
 const uint8_t INIT_TEXT[] = {
-  #include "text.inc"
+#include "text.inc"
 };
 
-uint8_t TEXT[HEIGHT / 8][WIDTH / 8];
+uint8_t TEXT[fb0_height / 8][fb0_width / 8];
 
-void render(uint16_t volatile *pixels, int pitch, int i)
+template <size_t HEIGHT, size_t STRIDE>
+void render(uint16_t volatile (&pixels)[HEIGHT][STRIDE], int WIDTH, int i)
 {
   for (int line = 0; line < HEIGHT; line++) {
-    uint16_t volatile *pixel_line = pixels + ((line + i) % HEIGHT)*pitch;
+    uint16_t volatile *pixel_line = pixels[(line + i) % HEIGHT];
 
     for (int col = 0; col < WIDTH; col++) {
       uint8_t c = TEXT[line / 8][col / 8];
@@ -43,7 +33,7 @@ void render(uint16_t volatile *pixels, int pitch, int i)
   }
 }
 
-}
+}  // namespace
 
 int main()
 {
@@ -52,7 +42,7 @@ int main()
   memcpy(&TEXT, &INIT_TEXT, sizeof(INIT_TEXT) - 1);
 
   for (int i = 0; true; i++) {
-    render(framebuffer_raw, framebuffer_stride / 2, i++);
+    render(fb0_pixels, fb0_width, i++);
   }
 
   return 0;
