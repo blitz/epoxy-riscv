9f383754fcaeab2e339202c1f82fb8a6ff74bfe8
Julian Stecklina <js@alien8.de>
2021-01-25 01:56:32 +0100

    Refactor release.nix

--- nix/release.nix
+++ nix/release.nix
@@ -24,26 +24,10 @@ rec {
 
   # This is the playground for the new Rust-based harden implementation.
   ulx3s-fbdemo =
-    let hardenCmd = "harden -r ${../config} -s ulx3s-saxonsoc-fbdemo -vvvv";
-    in
-    rec {
-      api = riscvPkgs.callPackage ./epoxy-api.nix { };
+    let
+      hardenCmd = "harden -r ${../config} -s ulx3s-saxonsoc-fbdemo -vvvv";
 
-      hello = riscvPkgs.callPackage ./epoxy-hello.nix { };
-
-      fbdemo = riscvPkgs.callPackage ./epoxy-fbdemo.nix {
-        resourceHeader = pkgs.runCommandNoCC "fbdemo-resources.hpp"
-          {
-            nativeBuildInputs = [ new-harden ];
-          } "${hardenCmd} configure-process fbdemo > $out";
-      };
-
-      user-binaries = riscvPkgs.symlinkJoin {
-        name = "user-binaries";
-        paths = [ fbdemo hello ];
-      };
-
-      kern-state = pkgs.runCommandNoCC "kern-state"
+      mkKernState = user-binaries: pkgs.runCommandNoCC "kern-state"
         {
           nativeBuildInputs = [ new-harden ];
         } ''
@@ -51,6 +35,39 @@ rec {
         ${hardenCmd} configure-kernel --header ${user-binaries} > $out/state.hpp
         ${hardenCmd} configure-kernel ${user-binaries} > $out/state.cpp
       '';
+
+      mkResourceHeader = procName: pkgs.runCommandNoCC "${procName}-resources.hpp"
+        {
+          nativeBuildInputs = [ new-harden ];
+        } "${hardenCmd} configure-process ${procName} > $out";
+
+      mkBootImage = kern: user-binaries: pkgs.runCommandNoCC "boot-image"
+        {
+          nativeBuildInputs = [ new-harden ];
+        } ''
+        mkdir -p $out/bin
+
+        ${hardenCmd} boot-image ${kern}/bin/epoxy-kern ${user-binaries} > $out/bin/epoxy-boot
+      '';
+
+    in
+    rec {
+      api = riscvPkgs.callPackage ./epoxy-api.nix { };
+
+      # TODO Use harden list-processes to figure out what user processes to build.
+
+      hello = riscvPkgs.callPackage ./epoxy-hello.nix { };
+
+      fbdemo = riscvPkgs.callPackage ./epoxy-fbdemo.nix {
+        resourceHeader = mkResourceHeader "fbdemo";
+      };
+
+      user-binaries = riscvPkgs.symlinkJoin {
+        name = "user-binaries";
+        paths = [ fbdemo hello ];
+      };
+
+      kern-state = mkKernState user-binaries;
 
       kern = riscvPkgs.callPackage ./epoxy-kern-new.nix {
         epoxy-api = api;
@@ -63,14 +80,7 @@ rec {
         paths = [ user-binaries kern ];
       };
 
-      boot-image = pkgs.runCommandNoCC "boot-image"
-        {
-          nativeBuildInputs = [ new-harden ];
-        } ''
-        mkdir -p $out/bin
-
-        ${hardenCmd} boot-image ${kern}/bin/epoxy-kern ${user-binaries} > $out/bin/epoxy-boot
-      '';
+      boot-image = mkBootImage kern user-binaries;
     };
 
   # TODO Use test.nix
