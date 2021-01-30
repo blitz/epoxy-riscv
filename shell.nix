a97d22a7b213e7d7b2b33d3402d2869744151250
Julian Stecklina <js@alien8.de>
2021-01-30 20:46:51 +0100

    Fix shell environment

--- shell.nix
+++ shell.nix
@@ -1,10 +1,10 @@
 { sources ? import ./nix/sources.nix, nixpkgs ? sources.nixpkgs
 , pkgs ? import nixpkgs { } }:
 
-let thisPackage = import ./nix/release.nix { inherit sources nixpkgs pkgs; };
-in thisPackage.riscvPkgs.mkShell {
-  inputsFrom = [ thisPackage.ulx3s-fbdemo.hello ];
+let local = import ./nix/release.nix { inherit sources nixpkgs pkgs; };
+in local.riscvPkgs.mkShell {
+  inputsFrom = [ local.systems.qemu-hello.kern ];
 
-  nativeBuildInputs = pkgs.lib.attrsets.mapAttrsToList (_: v: v) thisPackage.shellDependencies;
+  nativeBuildInputs = pkgs.lib.attrsets.mapAttrsToList (_: v: v) local.shellDependencies;
   buildInputs = [ pkgs.niv ];
 }
